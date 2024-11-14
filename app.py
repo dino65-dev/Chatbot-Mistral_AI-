9381d944fe5b8568606992a636d36b17eca2d273
DINMAY KUMAR BRAHMA <131891590+dino65-dev@users.noreply.github.com>
2024-11-14 09:10:36 +0000

    ...

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from langchain_mistralai import ChatMistralAI
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
-from langchain.chains import LLMChain 
+
 # Langchain settings
 load_dotenv(".env")
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_PROJECT"] = "mistral_ai"
+os.environ["LANGCHAIN_PROJECT"] = "app"
 
 # Initialize the LLM
 api_key = os.getenv("API_KEY")
@@ -22,12 +22,12 @@ llm = ChatMistralAI(model=mistral_model, temperature=0, api_key=api_key,token_li
 #sreamlit framework
 st.title("Chatbot(MistralAI)")
 input_text = st.text_input("Feel free to ask me anything")
-#output = llm.invoke([("user", input_text)])
+output = llm.invoke([("user", input_text)])
 
 # parse the output
 parser = StrOutputParser()
-result = LLMChain(llm=llm,prompt=input_text)
-output = parser.invoke(result)
+result = parser.invoke((output))
+
 # output
 if input_text:
-    st.write(output)
+    st.write(result)
